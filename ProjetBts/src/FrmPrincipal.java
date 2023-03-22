8cee7ccec5555399c6240f637196e09419ecc082
Alexandreeeeeeeee <alexandre.neves0402@gmail.com>
2023-03-22 13:26:06 +0100

    first push

---
+++ ProjetBts/src/FrmPrincipal.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class FrmPrincipal extends JFrame {
+    public FrmPrincipal(){
+
+        this.setTitle("PROJET");
+        this.setContentPane(rootPane);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setLocationRelativeTo(null);
+    }
+}
---
+++ ProjetBts/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+    FrmPrincipal frm = new FrmPrincipal();
+    frm.setVisible(true);
+    }
+}
